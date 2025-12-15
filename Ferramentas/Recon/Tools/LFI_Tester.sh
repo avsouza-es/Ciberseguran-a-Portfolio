f14fc58d516acad89ba46e0eb001d79089ada739
Alexandre <avsouza.es@gmail.com>
2025-12-15 08:54:06 -0300

    Update LFI_Tester.sh

--- Ferramentas/Recon/Tools/LFI_Tester.sh
+++ Ferramentas/Recon/Tools/LFI_Tester.sh
@@ -1,5 +1,31 @@
 #!/bin/bash
 
+# ==============================================================================
+# Título:       LFI_Tester.sh
+# Descrição:    Realiza um reconhecimento inicial (ping sweep e port scan) no alvo.
+# Autor:        Alexandre Vieira de Souza
+# Data:         2025-12-15
+# Versão:       1.0
+# Uso:          
+# Notas:        
+# ==============================================================================
+# LICENÇA (GPLv2):
+#
+# Este programa é software livre; você pode redistribuí-lo e/ou modificá-lo
+# sob os termos da Licença Pública Geral GNU conforme publicada pela
+# Free Software Foundation; tanto a versão 2 da Licença, como (a seu critério)
+# qualquer versão posterior.
+#
+# Este programa é distribuído na expectativa de que seja útil,
+# porém, SEM NENHUMA GARANTIA; nem mesmo a garantia implícita de
+# COMERCIABILIDADE ou ADEQUAÇÃO A UMA FINALIDADE ESPECÍFICA.
+# Consulte a Licença Pública Geral do GNU para mais detalhes.
+# ==============================================================================
+
+set -euo pipefail
+
+# --- Código começa aqui ---
+
 # ===============================================
 # Configuração
 # ===============================================
